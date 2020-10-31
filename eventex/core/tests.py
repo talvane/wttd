7bec91ce838766013b54c6fb859ed7561d0ac3a8
talvane <talvane.magalhaes@gmail.com>
2020-10-31 10:38:32 -0300

    enable subscribe

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -12,3 +12,7 @@ class HomeTest(TestCase):
     def test_template(self):
         """Must use index.html"""
         self.assertTemplateUsed(self.response, "index.html")
+
+    def test_subscription_link(self):
+        """Must be contains link inscricao"""
+        self.assertContains(self.response, 'href="/inscricao/"')
